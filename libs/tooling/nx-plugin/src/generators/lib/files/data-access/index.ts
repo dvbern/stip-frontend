2acd59aa86f4b60bed5e88253060532da7892930
tomastrajan <tomas.trajan@gmail.com>
2023-06-01 13:57:12 +0200

    feat(workspace): improve generated data-access service

--- libs/tooling/nx-plugin/src/generators/lib/files/data-access/index.ts
+++ libs/tooling/nx-plugin/src/generators/lib/files/data-access/index.ts
@@ -17,6 +17,6 @@ export * from './lib/<%= projectName %>.effects';
  * Search for // TODO and replace placeholder interfaces with real ones from a model lib
  *
  * If you need to access part of the state in multiple features then there are following options
- * 1) move feature registration to core (becomes eager and global, any feature can access its state)
+ * 1) move feature registration to app/core (becomes eager and global, any feature can access its state)
  * 2) create a new data-access library which is registered in core and manages subset of original state
  */
