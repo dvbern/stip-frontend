983a84bb499dd4597f3b929332567d2cffabfa78
nefr <frederic.nell@dvbern.ch>
2023-06-06 13:30:35 +0200

    KSTIP-56: Versucht zu definiren einen ersten Form Component zu verwenden in jede Form

--- libs/gesuch-app/feature/eltern/src/lib/gesuch-app-feature-eltern/gesuch-app-feature-eltern.component.ts
+++ libs/gesuch-app/feature/eltern/src/lib/gesuch-app-feature-eltern/gesuch-app-feature-eltern.component.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SharedUiProgressBarComponent} from "@dv/shared/ui/progress-bar";
 import {TranslateModule} from "@ngx-translate/core";
+import {SharedUiFormFieldComponent, SharedUiFormLabelTargetDirective} from "@dv/shared/ui/form-field";
+import {SharedUiDvFormComponentComponent} from "@dv/shared/ui/dv-form-component";
 
 @Component({
   selector: 'dv-gesuch-app-feature-eltern',
   standalone: true,
-    imports: [CommonModule, SharedUiProgressBarComponent, TranslateModule],
+  imports: [CommonModule, SharedUiProgressBarComponent, TranslateModule, SharedUiFormFieldComponent, SharedUiDvFormComponentComponent],
   templateUrl: './gesuch-app-feature-eltern.component.html',
   styleUrls: ['./gesuch-app-feature-eltern.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
