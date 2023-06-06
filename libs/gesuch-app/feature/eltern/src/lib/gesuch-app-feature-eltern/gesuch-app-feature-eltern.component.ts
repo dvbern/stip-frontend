6afa7ae05dd72e5c1caacb88c810b868b7025384
nefr <frederic.nell@dvbern.ch>
2023-06-06 11:07:38 +0200

    KSTIP-56: Add first Text Element mit Translator

--- libs/gesuch-app/feature/eltern/src/lib/gesuch-app-feature-eltern/gesuch-app-feature-eltern.component.ts
+++ libs/gesuch-app/feature/eltern/src/lib/gesuch-app-feature-eltern/gesuch-app-feature-eltern.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SharedUiProgressBarComponent} from "@dv/shared/ui/progress-bar";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'dv-gesuch-app-feature-eltern',
   standalone: true,
-  imports: [CommonModule, SharedUiProgressBarComponent],
+    imports: [CommonModule, SharedUiProgressBarComponent, TranslateModule],
   templateUrl: './gesuch-app-feature-eltern.component.html',
   styleUrls: ['./gesuch-app-feature-eltern.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
