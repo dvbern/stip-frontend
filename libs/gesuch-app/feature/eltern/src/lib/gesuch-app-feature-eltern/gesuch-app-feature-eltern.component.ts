fde8c14a58d5c46c4d819d5a0af43356978cbaf2
Andrea Brühlmann <andrea.bruehlmann@dvbern.ch>
2023-06-06 13:54:27 +0200

    KSTIP-61 Ausbildung-Formular

--- libs/gesuch-app/feature/eltern/src/lib/gesuch-app-feature-eltern/gesuch-app-feature-eltern.component.ts
+++ libs/gesuch-app/feature/eltern/src/lib/gesuch-app-feature-eltern/gesuch-app-feature-eltern.component.ts
@@ -1,14 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {SharedUiProgressBarComponent} from "@dv/shared/ui/progress-bar";
-import {TranslateModule} from "@ngx-translate/core";
-import {SharedUiFormFieldComponent, SharedUiFormLabelTargetDirective} from "@dv/shared/ui/form-field";
-import {SharedUiDvFormComponentComponent} from "@dv/shared/ui/dv-form-component";
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SharedUiFormFieldComponent } from '@dv/shared/ui/form-field';
+import { SharedUiProgressBarComponent } from '@dv/shared/ui/progress-bar';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'dv-gesuch-app-feature-eltern',
   standalone: true,
-  imports: [CommonModule, SharedUiProgressBarComponent, TranslateModule, SharedUiFormFieldComponent, SharedUiDvFormComponentComponent],
+  imports: [
+    CommonModule,
+    SharedUiProgressBarComponent,
+    TranslateModule,
+    SharedUiFormFieldComponent,
+  ],
   templateUrl: './gesuch-app-feature-eltern.component.html',
   styleUrls: ['./gesuch-app-feature-eltern.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
