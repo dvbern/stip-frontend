f1deb4dd5828287ea261f17a0f8d7ae31b8f5e92
nefr <frederic.nell@dvbern.ch>
2023-06-07 08:37:26 +0200

    KSTIP-56: Remove Global Component and Form Handler after talk with the team about the strategy for error handling form and labels, we keep it on the html in order to not inject the translator inside and keep a dynamic way to define custom error messages for each fields.

--- libs/gesuch-app/feature/eltern/src/lib/gesuch-app-feature-eltern/gesuch-app-feature-eltern.component.ts
+++ libs/gesuch-app/feature/eltern/src/lib/gesuch-app-feature-eltern/gesuch-app-feature-eltern.component.ts
@@ -2,22 +2,43 @@ import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SharedUiProgressBarComponent} from "@dv/shared/ui/progress-bar";
 import {TranslateModule} from "@ngx-translate/core";
-import {SharedUiFormFieldComponent, SharedUiFormLabelTargetDirective} from "@dv/shared/ui/form-field";
-import {ElternForm} from "./ElternForm";
-import {SharedUiDvFormComponentComponent} from "@dv/shared/ui/dv-form-component";
+import {SharedUiFormFieldComponent} from "@dv/shared/ui/form-field";
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 
 
 @Component({
   selector: 'dv-gesuch-app-feature-eltern',
   standalone: true,
-  imports: [CommonModule, SharedUiProgressBarComponent, TranslateModule, SharedUiFormFieldComponent, SharedUiDvFormComponentComponent, SharedUiDvFormComponentComponent],
+  imports: [CommonModule, ReactiveFormsModule, SharedUiProgressBarComponent, TranslateModule, SharedUiFormFieldComponent],
   templateUrl: './gesuch-app-feature-eltern.component.html',
   styleUrls: ['./gesuch-app-feature-eltern.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GesuchAppFeatureElternComponent {
+  private formBuilder = inject(FormBuilder);
 
-  form: ElternForm = new ElternForm();
+  form = this.formBuilder.group({
+          name: ['', [Validators.required]],
+          vorname: [undefined, Validators.required],
+          telefonnummer: [undefined],
+          sozialversicherungsnummer: [undefined],
+          geburtsdatum: [undefined],
+          sozialhilfebeitraegeAusbezahlt: [undefined],
+          ausweisbFluechtling: [undefined],
+          ergaenzungsleistungAusbezahlt: [undefined],
+        });
 
+  handleSaveAndContinue() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      //TODO navigate next and save
+    }
+  }
 
+  handleSaveAndBack() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      //TODO Navigate back and save
+    }
+  }
 }
--- libs/shared/ui/dv-form-component/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './lib/shared-ui-dv-form-component/shared-ui-dv-form-component.component';
