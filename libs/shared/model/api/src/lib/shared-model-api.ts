506e29fc235904acb1da0b320926fdc62e731282
nefr <frederic.nell@dvbern.ch>
2023-06-08 11:00:30 +0200

    KSTIP-56: prepare the required files to be able to work locally with the backend api instead of the mock

--- libs/shared/model/api/src/lib/shared-model-api.ts
+++ libs/shared/model/api/src/lib/shared-model-api.ts
@@ -1,5 +1,7 @@
 const SHARED_MODEL_API_PREFIX = 'api';
 const SHARED_MODEL_API_VERSION = 'v1';
 export const SHARED_MODEL_API_URL = `http://localhost:4300/${SHARED_MODEL_API_PREFIX}/${SHARED_MODEL_API_VERSION}`;
+// Uncomment if you want to use the backend API instead of the mock
+//export const SHARED_MODEL_API_URL = `http://localhost:8080/${SHARED_MODEL_API_PREFIX}/${SHARED_MODEL_API_VERSION}`;
 
 export const SHARED_MODEL_ELTERN_RESOURCE = `/eltern`;
