304edca268203296da9a9bb642368401e8241fd2
nefr <frederic.nell@dvbern.ch>
2023-06-27 13:25:02 +0200

    KSTIP-200: Nationalitaet Wahl und auswirkungen

--- libs/gesuch-app/feature/gesuch-form-person/src/lib/gesuch-app-feature-gesuch-form-person/gesuch-app-feature-gesuch-form-person.component.ts
+++ libs/gesuch-app/feature/gesuch-form-person/src/lib/gesuch-app-feature-gesuch-form-person/gesuch-app-feature-gesuch-form-person.component.ts
@@ -22,6 +22,7 @@ import { selectLanguage } from '@dv/shared/data-access/language';
 import {
   Anrede,
   MASK_SOZIALVERSICHERUNGSNUMMER,
+  Niederlassungsstatus,
   PATTERN_EMAIL,
   PersonInAusbildungDTO,
   SharedModelGesuch,
@@ -89,14 +90,13 @@ const MEDIUM_AGE_GESUCHSSTELLER = 20;
 export class GesuchAppFeatureGesuchFormPersonComponent implements OnInit {
   private store = inject(Store);
   private formBuilder = inject(FormBuilder);
-
   readonly MASK_SOZIALVERSICHERUNGSNUMMER = MASK_SOZIALVERSICHERUNGSNUMMER;
-
   readonly PATTERN_EMAIL = PATTERN_EMAIL;
   readonly Anrede = Anrede;
   readonly Zivilstand = Zivilstand;
   readonly zivilstandValues = Object.values(Zivilstand);
   readonly Wohnsitz = Wohnsitz;
+  readonly niederlassungStatus = Object.values(Niederlassungsstatus);
   laenderSig = computed(() => {
     return this.view().laender;
   });
@@ -147,6 +147,7 @@ export class GesuchAppFeatureGesuchFormPersonComponent implements OnInit {
     ],
     nationalitaet: ['', [Validators.required]],
     heimatort: ['', [Validators.required]],
+    niederlassungsstatus: ['', [Validators.required]],
     vormundschaft: new FormControl<boolean | null>(null, []),
     zivilstand: ['', [Validators.required]],
     wohnsitz: ['', [Validators.required]],
@@ -156,8 +157,6 @@ export class GesuchAppFeatureGesuchFormPersonComponent implements OnInit {
   });
 
   constructor() {
-    console.log(this.zivilstandValues);
-    console.log(this.zivilstandValues[0]);
     effect(
       () => {
         const { gesuch } = this.view();
@@ -193,6 +192,30 @@ export class GesuchAppFeatureGesuchFormPersonComponent implements OnInit {
       this.form.controls.zivilrechtlicherWohnsitzPlz.updateValueAndValidity();
       this.form.controls.zivilrechtlicherWohnsitzOrt.updateValueAndValidity();
     });
+
+    const nationalitaetChanged$ = toSignal(
+      this.form.controls.nationalitaet.valueChanges
+    );
+    effect(
+      () => {
+        const nationalitaetChanged = nationalitaetChanged$();
+        if (this.form.controls.nationalitaet.value === 'CH') {
+          this.form.controls.heimatort.enable();
+          this.form.controls.niederlassungsstatus.patchValue('');
+          this.form.controls.niederlassungsstatus.disable();
+        } else if (nationalitaetChanged === undefined) {
+          this.form.controls.niederlassungsstatus.patchValue('');
+          this.form.controls.niederlassungsstatus.disable();
+          this.form.controls.heimatort.patchValue('');
+          this.form.controls.heimatort.disable();
+        } else {
+          this.form.controls.heimatort.patchValue('');
+          this.form.controls.heimatort.disable();
+          this.form.controls.niederlassungsstatus.enable();
+        }
+      },
+      { allowSignalWrites: true }
+    );
   }
 
   ngOnInit() {
