2b2c3f3292c5df82a3415061f94f9b6c67873e57
Andrea Brühlmann <andrea.bruehlmann@dvbern.ch>
2023-07-05 12:45:47 +0200

    KSTIP-195 E-Mail-Validierung mit Tests

--- libs/shared/model/gesuch/src/lib/shared-model-gesuch.ts
+++ libs/shared/model/gesuch/src/lib/shared-model-gesuch.ts
@@ -12,65 +12,7 @@ export interface SharedModelGesuch extends GesuchDTO {
 export const SHARED_MODEL_GESUCH_RESOURCE = `/gesuch`;
 export const SHARED_MODEL_GESUCHSPERIODE_RESOURCE = `/gesuchsperiode`;
 
-export const MASK_SOZIALVERSICHERUNGSNUMMER: MaskitoOptions = {
-  mask: [
-    /\d/,
-    /\d/,
-    /\d/,
-    '.',
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    '.',
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    '.',
-    /\d/,
-    /\d/,
-  ],
-};
-
-export const MASK_IBAN: MaskitoOptions = {
-  mask: [
-    'C',
-    'H',
-    /\d/,
-    /\d/,
-    ' ',
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    ' ',
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    ' ',
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    ' ',
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    ' ',
-    /\d/,
-  ],
-};
-
-export const MASK_MM_YYYY: MaskitoOptions = {
-  mask: [/\d/, /\d/, '.', /\d/, /\d/, /\d/, /\d/],
-};
-
 export enum GesuchsperiodeSemester {
   HERBST = 'HERBST',
   FRUEHLING = 'FRUEHLING',
 }
-
-export const PATTERN_EMAIL = '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
