a6078957f3607b1034b1c93b659d53cef7e0c340
Philipp Schärer <philipp.schaerer@dvbern.ch>
2023-07-10 15:35:25 +0200

    fix: KSTIP-233 removed unecessary div
    Also centered the save button on mobile

--- libs/gesuch-app/ui/step-form-buttons/src/lib/gesuch-app-ui-step-form-buttons/gesuch-app-ui-step-form-buttons.component.ts
+++ libs/gesuch-app/ui/step-form-buttons/src/lib/gesuch-app-ui-step-form-buttons/gesuch-app-ui-step-form-buttons.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,4 +9,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./gesuch-app-ui-step-form-buttons.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GesuchAppUiStepFormButtonsComponent {}
+export class GesuchAppUiStepFormButtonsComponent {
+  @HostBinding('class') classes =
+    'col-12 col-xl-8 mt-5 d-flex flex-column gap-3 flex-md-row align-items-center justify-content-center';
+}
