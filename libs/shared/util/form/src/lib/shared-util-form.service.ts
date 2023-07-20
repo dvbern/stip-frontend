c82275e8f7d743a3b41bc3d65f3fdb5abf1004f3
Philipp Schärer <philipp.schaerer@dvbern.ch>
2023-07-20 17:21:25 +0200

    fix: KSTIP-309 update default validity correctly in person

--- libs/shared/util/form/src/lib/shared-util-form.service.ts
+++ libs/shared/util/form/src/lib/shared-util-form.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SharedUtilFormService {
+  /**
+   * Used to set the disabled state of the given control
+   */
   setDisabledState(
     control: FormControl,
     isDisabled: boolean,
@@ -20,6 +24,10 @@ export class SharedUtilFormService {
       control.enable(options);
     }
   }
+
+  /**
+   * Used to add or remove the required validators, validity checks are also triggered afterwards
+   */
   setRequired(control: FormControl, required: boolean) {
     if (required) {
       control.addValidators(Validators.required);
@@ -27,5 +35,27 @@ export class SharedUtilFormService {
       control.removeValidators(Validators.required);
     }
     control.updateValueAndValidity();
+  }
+
+  /**
+   * Convert the value changes from a given control into a signal
+   */
+  signalFromChanges<R>(control: FormControl<R>): ReturnType<typeof toSignal<R>>;
+  /**
+   * Convert the value changes from a given control into a signal with default values
+   */
+  signalFromChanges<R>(
+    control: FormControl<R>,
+    opts: { useDefault: boolean }
+  ): ReturnType<typeof toSignal<R, R>>;
+  signalFromChanges<R>(
+    control: FormControl<R>,
+    opts?: { useDefault?: boolean }
+  ) {
+    return opts?.useDefault
+      ? toSignal<R, R>(control.valueChanges, {
+          initialValue: control.defaultValue,
+        })
+      : toSignal<R>(control.valueChanges);
   }
 }
