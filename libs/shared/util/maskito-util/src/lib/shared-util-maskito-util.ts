828c2972d86a693003fe16af9f75372a585aa77d
Philipp Schärer <philipp.schaerer@dvbern.ch>
2023-08-14 11:28:53 +0200

    fix: use number separator

--- libs/shared/util/maskito-util/src/lib/shared-util-maskito-util.ts
+++ libs/shared/util/maskito-util/src/lib/shared-util-maskito-util.ts
@@ -1,8 +1,19 @@
 import { maskitoNumberOptionsGenerator } from '@maskito/kit';
 
+export const NUMBER_THOUSAND_SEPARATOR = "'";
+
 export const maskitoPercent = maskitoNumberOptionsGenerator({
   postfix: '%',
   min: 0,
   max: 100,
   precision: 2,
 });
+
+export const maskitoNumber = maskitoNumberOptionsGenerator({
+  min: 0,
+  thousandSeparator: NUMBER_THOUSAND_SEPARATOR,
+});
+
+export const fromFormatedNumber = (formatedNumber: string): number | null => {
+  return +formatedNumber.replace(NUMBER_THOUSAND_SEPARATOR, '');
+};
