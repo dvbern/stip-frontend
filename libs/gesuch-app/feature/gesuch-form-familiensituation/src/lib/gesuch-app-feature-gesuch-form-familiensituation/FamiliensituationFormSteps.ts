bb7166a7c4e46b489a42cb98b4b124260a195dbd
Simon Kafader <simon.kafader@dvbern.ch>
2023-09-06 11:02:59 +0200

    feature(familiensituation) KSTIP-205: adapt FamiliensituationFormSteps to only have four steps

--- libs/gesuch-app/feature/gesuch-form-familiensituation/src/lib/gesuch-app-feature-gesuch-form-familiensituation/FamiliensituationFormSteps.ts
+++ libs/gesuch-app/feature/gesuch-form-familiensituation/src/lib/gesuch-app-feature-gesuch-form-familiensituation/FamiliensituationFormSteps.ts
@@ -8,6 +8,7 @@ export interface FamiliensituationFormStep {
   getPrevious(
     familienstituation: FamiliensituationUpdate
   ): FamiliensituationFormStep;
+  name: string;
 }
 
 // Helper function to ensure type safety in our map
@@ -17,6 +18,7 @@ function createFamSitStep(famStiS: FamiliensituationFormStep) {
 
 export const FamiliensituationFormSteps = {
   ELTERN_VERHEIRATET_ZUSAMMEN: createFamSitStep({
+    name: 'ELTERN_VERHEIRATET_ZUSAMMEN',
     getNext(
       familiensituation: FamiliensituationUpdate
     ): FamiliensituationFormStep {
@@ -33,151 +35,35 @@ export const FamiliensituationFormSteps = {
   }),
 
   ALIMENTENREGELUNG: createFamSitStep({
+    name: 'ALIMENTENREGELUNG',
     getNext(
       familiensituation: FamiliensituationUpdate
     ): FamiliensituationFormStep {
-      if (familiensituation.gerichtlicheAlimentenregelung === null) {
-        return this;
-      }
-      return familiensituation.gerichtlicheAlimentenregelung
-        ? FamiliensituationFormSteps.WER_ZAHLT_ALIMENTE
-        : FamiliensituationFormSteps.ELTERN_UNBEKANNT_VERSTORBEN;
+      return familiensituation.gerichtlicheAlimentenregelung === false
+        ? FamiliensituationFormSteps.ELTERN_UNBEKANNT_VERSTORBEN
+        : this;
     },
     getPrevious(): FamiliensituationFormStep {
       return FamiliensituationFormSteps.ELTERN_VERHEIRATET_ZUSAMMEN;
     },
   }),
 
-  WER_ZAHLT_ALIMENTE: createFamSitStep({
-    getNext(
-      familiensituation: FamiliensituationUpdate
-    ): FamiliensituationFormStep {
-      if (
-        familiensituation.werZahltAlimente === null ||
-        familiensituation.werZahltAlimente === 'GEMEINSAM'
-      ) {
-        return this;
-      }
-      return familiensituation.werZahltAlimente === 'VATER'
-        ? FamiliensituationFormSteps.MUTTER_WIEDERVERHEIRATET
-        : FamiliensituationFormSteps.VATER_WIEDERVERHEIRATET;
-    },
-    getPrevious(): FamiliensituationFormStep {
-      return FamiliensituationFormSteps.ALIMENTENREGELUNG;
-    },
-  }),
-
-  VATER_WIEDERVERHEIRATET: createFamSitStep({
-    getNext(): FamiliensituationFormStep {
-      return this;
-    },
-    getPrevious(
-      familiensituation: FamiliensituationUpdate
-    ): FamiliensituationFormStep {
-      if (familiensituation.werZahltAlimente === 'MUTTER') {
-        return FamiliensituationFormSteps.WER_ZAHLT_ALIMENTE;
-      }
-      if (familiensituation.mutterUnbekanntGrund !== null) {
-        return FamiliensituationFormSteps.WIESO_MUTTER_UNBEKANNT;
-      }
-      return FamiliensituationFormSteps.WELCHER_ELTERNTEIL_UNBEKANNT_VERSTORBEN;
-    },
-  }),
-
-  MUTTER_WIEDERVERHEIRATET: createFamSitStep({
-    getNext(): FamiliensituationFormStep {
-      return this;
-    },
-    getPrevious(
-      familiensituation: FamiliensituationUpdate
-    ): FamiliensituationFormStep {
-      if (familiensituation.werZahltAlimente === 'VATER') {
-        return FamiliensituationFormSteps.WER_ZAHLT_ALIMENTE;
-      }
-      if (familiensituation.vaterUnbekanntGrund !== null) {
-        return FamiliensituationFormSteps.WIESO_VATER_UNBEKANNT;
-      }
-      return FamiliensituationFormSteps.WELCHER_ELTERNTEIL_UNBEKANNT_VERSTORBEN;
-    },
-  }),
-
-  WIESO_VATER_UNBEKANNT: createFamSitStep({
-    getNext(
-      familiensituation: FamiliensituationUpdate
-    ): FamiliensituationFormStep {
-      return familiensituation.vaterUnbekanntGrund === null
-        ? this
-        : FamiliensituationFormSteps.MUTTER_WIEDERVERHEIRATET;
-    },
-    getPrevious(): FamiliensituationFormStep {
-      return FamiliensituationFormSteps.WELCHER_ELTERNTEIL_UNBEKANNT_VERSTORBEN;
-    },
-  }),
-
-  WIESO_MUTTER_UNBEKANNT: createFamSitStep({
-    getNext(
-      familiensituation: FamiliensituationUpdate
-    ): FamiliensituationFormStep {
-      return familiensituation.mutterUnbekanntGrund === null
-        ? this
-        : FamiliensituationFormSteps.VATER_WIEDERVERHEIRATET;
-    },
-    getPrevious(): FamiliensituationFormStep {
-      return FamiliensituationFormSteps.WELCHER_ELTERNTEIL_UNBEKANNT_VERSTORBEN;
-    },
-  }),
-
   ELTERN_UNBEKANNT_VERSTORBEN: createFamSitStep({
+    name: 'ELTERN_UNBEKANNT_VERSTORBEN',
     getNext(
       familiensituation: FamiliensituationUpdate
     ): FamiliensituationFormStep {
-      if (familiensituation.elternteilUnbekanntVerstorben === null) {
-        return this;
-      }
-      return familiensituation.elternteilUnbekanntVerstorben
-        ? FamiliensituationFormSteps.WELCHER_ELTERNTEIL_UNBEKANNT_VERSTORBEN
-        : FamiliensituationFormSteps.ZWEI_FAMILIENBUDGET;
+      return familiensituation.elternteilUnbekanntVerstorben === false
+        ? FamiliensituationFormSteps.ZWEI_FAMILIENBUDGET
+        : this;
     },
     getPrevious(): FamiliensituationFormStep {
       return FamiliensituationFormSteps.ALIMENTENREGELUNG;
-    },
-  }),
-
-  WELCHER_ELTERNTEIL_UNBEKANNT_VERSTORBEN: createFamSitStep({
-    getNext(
-      familiensituation: FamiliensituationUpdate
-    ): FamiliensituationFormStep {
-      if (
-        familiensituation.vaterUnbekanntVerstorben === null ||
-        familiensituation.mutterUnbekanntVerstorben === null
-      ) {
-        return this;
-      }
-      if (
-        familiensituation.vaterUnbekanntVerstorben === 'WEDER_NOCH' &&
-        familiensituation.mutterUnbekanntVerstorben !== 'WEDER_NOCH'
-      ) {
-        return familiensituation.mutterUnbekanntVerstorben === 'UNBEKANNT'
-          ? FamiliensituationFormSteps.WIESO_MUTTER_UNBEKANNT
-          : FamiliensituationFormSteps.VATER_WIEDERVERHEIRATET;
-      }
-
-      if (
-        familiensituation.mutterUnbekanntVerstorben === 'WEDER_NOCH' &&
-        familiensituation.vaterUnbekanntVerstorben !== 'WEDER_NOCH'
-      ) {
-        return familiensituation.vaterUnbekanntVerstorben === 'UNBEKANNT'
-          ? FamiliensituationFormSteps.WIESO_VATER_UNBEKANNT
-          : FamiliensituationFormSteps.MUTTER_WIEDERVERHEIRATET;
-      }
-      return this;
-    },
-    getPrevious(): FamiliensituationFormStep {
-      return FamiliensituationFormSteps.ELTERN_UNBEKANNT_VERSTORBEN;
     },
   }),
 
   ZWEI_FAMILIENBUDGET: createFamSitStep({
+    name: 'ZWEI_FAMILIENBUDGET',
     getNext(): FamiliensituationFormStep {
       return this;
     },
