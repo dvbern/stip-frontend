4361e0501bb912c32854773c00d9e8cae3845f9c
Philipp Schärer <philipp.schaerer@dvbern.ch>
2023-09-08 15:59:11 +0200

    fix(ci): another try to build SB

--- apps/sachbearbeitung-app/src/main.ts
+++ apps/sachbearbeitung-app/src/main.ts
@@ -4,7 +4,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// TODO: cleanup, used to build affected +3
+// TODO: cleanup, used to build affected +4
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
