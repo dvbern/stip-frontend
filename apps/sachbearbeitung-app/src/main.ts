b38fea73a18ea2f6470575ce16266a695a759369
Philipp Schärer <philipp.schaerer@dvbern.ch>
2023-09-08 15:34:29 +0200

    chore(ci): trigger affected build of SB

--- apps/sachbearbeitung-app/src/main.ts
+++ apps/sachbearbeitung-app/src/main.ts
@@ -4,7 +4,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// TODO: cleanup, used to build affected
+// TODO: cleanup, used to build affected +1
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
