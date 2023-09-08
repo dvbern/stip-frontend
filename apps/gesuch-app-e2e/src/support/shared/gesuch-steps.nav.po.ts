0134e3dedd8426483dcb2d369ecff5bb8a43f987
Philipp Schärer <philipp.schaerer@dvbern.ch>
2023-09-08 10:56:15 +0200

    fix: e2e after SB merge

--- apps/gesuch-app-e2e/src/support/shared/gesuch-steps.nav.po.ts
+++ apps/gesuch-app-e2e/src/support/shared/gesuch-steps.nav.po.ts
@@ -1,7 +1,6 @@
 export const getNavDashboard = () => cy.getBySel('gesuch-step-nav-dashboard');
 
-export const getStep = (step: string) =>
-  cy.getBySel('step-nav-gesuch-app-feature-gesuch-form-' + step);
+export const getStep = (step: string) => cy.getBySel('step-nav-' + step);
 
 export const getStepPersonInAusbildung = () => getStep('person');
 export const getStepLebenslauf = () => getStep('lebenslauf');
