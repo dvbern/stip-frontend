70e33c5c7e3c1455b734e4659ba66641d67b69d7
Schärer Philipp <philipp.schaerer@dvbern.ch>
2023-09-11 15:32:15 +0200

    fix(ci): trigger deployment of GS & SB

--- apps/gesuch-app/src/main.ts
+++ apps/gesuch-app/src/main.ts
@@ -3,6 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// TODO: cleanup, used to build affected +1
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
