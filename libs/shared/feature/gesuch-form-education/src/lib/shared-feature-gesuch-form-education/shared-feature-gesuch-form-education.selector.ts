6d9eec6bd3046467717815ed3de5b174c68c31f1
Philipp Schärer <philipp.schaerer@dvbern.ch>
2023-10-09 16:08:22 +0200

    fix(KSTIP-378): ausbildung min gesuchperiode

--- libs/shared/feature/gesuch-form-education/src/lib/shared-feature-gesuch-form-education/shared-feature-gesuch-form-education.selector.ts
+++ libs/shared/feature/gesuch-form-education/src/lib/shared-feature-gesuch-form-education/shared-feature-gesuch-form-education.selector.ts
@@ -2,14 +2,25 @@ import { createSelector } from '@ngrx/store';
 import { selectSharedDataAccessGesuchsView } from '@dv/shared/data-access/gesuch';
 import { selectSharedDataAccessAusbildungsstaettesView } from '@dv/shared/data-access/ausbildungsstaette';
 
+import { format, startOfYear } from 'date-fns';
+
 export const selectSharedFeatureGesuchFormEducationView = createSelector(
   selectSharedDataAccessGesuchsView,
   selectSharedDataAccessAusbildungsstaettesView,
-  (gesuchsView, ausbildungsstaettesView) => ({
-    loading: gesuchsView.loading || ausbildungsstaettesView.loading,
-    gesuch: gesuchsView.gesuch,
-    gesuchFormular: gesuchsView.gesuchFormular,
-    ausbildung: gesuchsView.gesuchFormular?.ausbildung,
-    ausbildungsstaettes: ausbildungsstaettesView.ausbildungsstaettes,
-  })
+  (gesuchsView, ausbildungsstaettesView) => {
+    const gesuchsPeriodenStart = gesuchsView.gesuch
+      ? startOfYear(new Date(gesuchsView.gesuch.gesuchsperiode.gueltigAb))
+      : null;
+    return {
+      loading: gesuchsView.loading || ausbildungsstaettesView.loading,
+      gesuch: gesuchsView.gesuch,
+      gesuchFormular: gesuchsView.gesuchFormular,
+      ausbildung: gesuchsView.gesuchFormular?.ausbildung,
+      ausbildungsstaettes: ausbildungsstaettesView.ausbildungsstaettes,
+      gesuchsPeriodenStart,
+      gesuchsPeriodenStartFormatted: gesuchsPeriodenStart
+        ? format(gesuchsPeriodenStart, 'yyyy')
+        : null,
+    };
+  }
 );
